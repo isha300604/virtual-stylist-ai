dad18bad7e9fab426903df666e703edf8a99adee
isha300604 <ishakatkar6@gmail.com>
2026-01-30 00:11:40 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,23 +4,21 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
+
   return {
     server: {
       port: 3000,
       host: '0.0.0.0',
     },
-    preview: {
-      allowedHosts: 'all'   // 👈 THIS LINE FIXES RENDER
-    },
     plugins: [react()],
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
-      }
-    }
+      },
+    },
   };
 });
